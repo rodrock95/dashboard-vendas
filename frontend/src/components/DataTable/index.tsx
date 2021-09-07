cba0395dc01644b0fcf68c75901bf22c8a3a1ef4
rodrock95 <r.barboza9502@gmail.com>
2021-09-07 20:26:57 -0300

    Static charts

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -39,37 +39,43 @@ const DataTable = () => {   //function NavBar() {
                         <td>34</td>
                         <td>25</td>
                         <td>15017.00</td>
-                    </tr><tr>
+                    </tr>
+                    <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
                         <td>15017.00</td>
-                    </tr><tr>
+                    </tr>
+                    <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
                         <td>15017.00</td>
-                    </tr><tr>
+                    </tr>
+                    <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
                         <td>15017.00</td>
-                    </tr><tr>
+                    </tr>
+                    <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
                         <td>15017.00</td>
-                    </tr><tr>
+                    </tr>
+                    <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
                         <td>15017.00</td>
-                    </tr><tr>
+                    </tr>
+                    <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
